5dce703af12f8e7604d2d322b616cd6b3f0e2959
Dan Redmond <dan.w.redmond@gmail.com>
2014-08-20 22:15:18 -0400

    add figaro and stripe stuff

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'bootstrap-sass'
 
 gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
 
+gem "figaro"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+
+  resource :charges
+  
   root 'pages#home'
+
 
   
   # The priority is based upon order of creation: first created -> highest priority.
