31b3fbcb4a8c46da1c08c7bce78a63efd453dfbb
Dan Redmond <dan.w.redmond@gmail.com>
2014-08-27 07:42:47 -0400

    mailer

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,6 @@
 class Purchase < ActiveRecord::Base
 
-  # after_create :email_purchaser
+  after_create :email_purchaser
 
 
   def to_param
@@ -12,8 +12,8 @@ class Purchase < ActiveRecord::Base
 
 private
 
-def email_purchaser
-   PurchaseMailer.purchase_receipt(self).deliver
-end
+  def email_purchaser
+     PurchaseMailer.purchase_receipt(self).deliver
+  end
 
 end
